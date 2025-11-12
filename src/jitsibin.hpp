2e2719af1afd1eee2a7e293e7db17cf1595b9bf8
danryu <dan.garton@gmail.com>
2025-11-12 14:54:59 +0200

    remove breaky cruft

--- src/jitsibin.hpp
+++ src/jitsibin.hpp
@@ -32,8 +32,5 @@ struct GstJitsiBinClass {
 
 GType gst_jitsibin_get_type(void);
 
-// Runtime control helpers
-void gst_jitsibin_set_source_max_height(GstJitsiBin* bin, const char* source_name, int max_height);
-
 G_END_DECLS
 }
